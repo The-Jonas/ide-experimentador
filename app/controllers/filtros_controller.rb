abf93f229e97658df5a403f6d6b16a82175057d1
João Victor Pereira <institutodnb@gmail.com>
2023-12-14 16:39:24 -0300

    Filtro Funcionando

--- app/controllers/filtros_controller.rb
+++ app/controllers/filtros_controller.rb
@@ -107,7 +107,7 @@ class FiltrosController < ApplicationController
 
                 if !lista_de_testes_tags.any? { |lista| lista[0] == nome_do_experimento && lista[1] == nome_do_teste }
                     lista_de_testes_tags << [nome_do_experimento, nome_do_teste] 
-                    lista_de_tags_por_indice << []
+                    lista_de_tags_por_indice << [""]
                 end
             end
 
@@ -167,9 +167,9 @@ class FiltrosController < ApplicationController
             @pegativo = params[:ativos]
 
             if @pegativo == "Habilitado"
-                @pegativo = "false"
+                @pegativo = false
             elsif @pegativo == 'Desabilitado'
-                @pegativo = "true"
+                @pegativo = true
             end
             
         end
@@ -177,15 +177,15 @@ class FiltrosController < ApplicationController
         #puts "Experimento rodando #{@selected_experimento} e a lista #{@todos_os_experimentos}"
         #puts "Status: #{@pegaStatus}, Ativo: #{@pegativo}, Bateria: #{@pegaBateria}, Tag: #{@pegaTag} e Coordenadas #{@pegaOx}, #{@pegaOy}, #{@pegaOz}"
 
-        printar_no_final = []
+        @printar_no_final = []
         contador = 0 
 
-        printar_no_final << ['Teste', 'Ativo', 'Status', 'Bateria', 'Tag', 'X', 'Y', 'Z']
+        @printar_no_final << ['Teste', 'Ativo', 'Status', 'Bateria', 'Tag', 'X', 'Y', 'Z']
 
         lista_de_testes_ativos.each do |ativo|
     
             if ativo[0] == @selected_experimento
-                printar_no_final[contador][0] = ativo[1]
+                @printar_no_final[contador][0] = ativo[1]
                 nome_teste_atual = ativo[1]
                 index_a = lista_de_testes_ativos.find_index(ativo)
                 #puts "O valor do indice é: #{dabliu}"
@@ -194,47 +194,48 @@ class FiltrosController < ApplicationController
 
                 index_s = lista_de_testes_status.find_index do |lista|
                     lista[0] == @selected_experimento && lista[1] == nome_teste_atual
-                    break
                 end
 
                 index_b = lista_de_testes_bateria.find_index do |lista|
                     lista[0] == @selected_experimento && lista[1] == nome_teste_atual
-                    break
                 end
 
                 index_t = lista_de_testes_tags.find_index do |lista|
                     lista[0] == @selected_experimento && lista[1] == nome_teste_atual
-                    break
                 end
                 
                 index_c = lista_de_testes_coordenadas.find_index do |lista|
                     lista[0] == @selected_experimento && lista[1] == nome_teste_atual
-                    break
                 end
+
+                puts "Esse é o #{nome_teste_atual} do #{@selected_experimento}"
+                puts "lista de ativos #{lista_habilitado_por_indice}"
+                puts "lista de tags #{lista_de_tags_por_indice}"
                 
 
-                if @pegativos == "" || lista_habilitado_por_indice[index_a][0] == @pegativos
-                    printar_no_final[contador][1] = lista_habilitado_por_indice[index_a][0]
 
-                    if @pegaStatus == ""|| lista_de_status[index_s][0] == @pegaStatus
-                        printar_no_final[contador][2] = lista_de_status[index_s][0]
-                        
+                if @pegativo.nil? || @pegativo == "" || lista_habilitado_por_indice[index_a][0] == @pegativo
+                    @printar_no_final[contador][1] = lista_habilitado_por_indice[index_a][0]
+
+                    if @pegaStatus.to_s == ""|| lista_de_status[index_s][0] == @pegaStatus
+                        @printar_no_final[contador][2] = lista_de_status[index_s][0]
+
                         if @pegaBateria == ""|| lista_de_baterias_por_indice[index_b][0] == @pegaBateria
-                            printar_no_final[contador][3] = lista_de_baterias_por_indice[index_b][0]
+                            @printar_no_final[contador][3] = lista_de_baterias_por_indice[index_b][0]
                             
                             if @pegaTag == ""|| lista_de_tags_por_indice[index_t][0] == @pegaTag
-                                printar_no_final[contador][4] = lista_de_tags_por_indice[index_t][0]
+                                @printar_no_final[contador][4] = lista_de_tags_por_indice[index_t]
                                 
                                 if @pegaOx == ""|| lista_de_coordenadas_por_indice[index_c][0] == @pegaOx
-                                    printar_no_final[contador][5] = lista_de_coordenadas_por_indice[index_c][0]
+                                    @printar_no_final[contador][5] = lista_de_coordenadas_por_indice[index_c][0]
                                     
                                     if @pegaOy == ""|| lista_de_coordenadas_por_indice[index_c][1] == @pegaOy
-                                        printar_no_final[contador][6] = lista_de_coordenadas_por_indice[index_c][1]
+                                        @printar_no_final[contador][6] = lista_de_coordenadas_por_indice[index_c][1]
                                         
                                         if @pegaOz == ""|| lista_de_coordenadas_por_indice[index_c][2] == @pegaOz
-                                            printar_no_final[contador][7] = lista_de_coordenadas_por_indice[index_c][2]
+                                            @printar_no_final[contador][7] = lista_de_coordenadas_por_indice[index_c][2]
                                             contador += 1
-                                            printar_no_final << ['Teste', 'Ativo', 'Status', 'Bateria', 'Tag', 'X', 'Y', 'Z']
+                                            @printar_no_final << ['Teste', 'Ativo', 'Status', 'Bateria', 'Tag', 'X', 'Y', 'Z']
                                         end
                                     end
                                 end
@@ -245,14 +246,12 @@ class FiltrosController < ApplicationController
             end
         end
 
-        if printar_no_final[contador][7] == 'Z'
-            printar_no_final.pop
+        if @printar_no_final[contador][7] == 'Z'
+            @printar_no_final.pop
         end
-        printar_no_final.each do [teste]
-            puts "Teste: #{teste}"
+        @printar_no_final.each do |caixinha|
+            puts "Teste: #{caixinha}"
         end
-        puts "Lista_de_testes: #{printar_no_final}"
-    end    
-end 
-
-
+        puts "Lista_de_testes: #{@printar_no_final}"
+    end
+end
